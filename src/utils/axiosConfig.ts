a02c03df6462776e7887b2a5ed9706bfd7c2eb45
AlaaAlkhfagi <155472398+AlaaAlkhfagi@users.noreply.github.com>
2024-10-11 00:00:54 +0300

    df

--- src/utils/axiosConfig.ts
+++ src/utils/axiosConfig.ts
@@ -3,8 +3,8 @@ import axios from "axios";
 // Determine which API URL to use based on the environment
 const apiUrl =
   process.env.NODE_ENV === "production"
-    ? process.env.NEXT_PUBLIC_PRODUCTION_DOMAIN
-    : process.env.NEXT_PUBLIC_DEVELOPMENT_DOMAIN;
+    ? process.env.NEXT_PUBLIC_PRODUCTION_DOMAIN // Use production domain in production
+    : process.env.NEXT_PUBLIC_DEVELOPMENT_DOMAIN; // Use development domain in development
 
 console.log("API URL being used:", apiUrl);
 
