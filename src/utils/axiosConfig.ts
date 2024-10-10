0b187e58d077da2d042b176312bcc69d6eb4f32d
AlaaAlkhfagi <155472398+AlaaAlkhfagi@users.noreply.github.com>
2024-10-10 23:13:27 +0300

    Update axiosConfig.ts

---
+++ src/utils/axiosConfig.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+// Determine which API URL to use based on the environment
+const apiUrl =
+  process.env.NODE_ENV === "production"
+    ? process.env.NEXT_PUBLIC_PRODUCTION_DOMAIN
+    : process.env.NEXT_PUBLIC_DEVELOPMENT_DOMAIN;
+
+console.log("API URL being used:", apiUrl);
+
+const axiosInstance = axios.create({
+  baseURL: apiUrl,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default axiosInstance;
