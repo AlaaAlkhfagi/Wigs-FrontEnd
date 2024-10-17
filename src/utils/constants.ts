cc5df9aa8b1d36d817a397981f897ee433c2c1e9
AlaaAlkhfagi <155472398+AlaaAlkhfagi@users.noreply.github.com>
2024-10-17 04:07:47 +0300

    ss

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,9 @@
-export const ARTICLE_PER_PAGE = 10;
+export const ARTICLE_PER_PAGE = process.env.NEXT_PUBLIC_ARTICLE_PER_PAGE || 10;
 
-const PRODUCTION_DOMAIN = "http://localhost:5000/api/v1";
-const DEVELOPMENT_DOMAIN = "http://localhost:5000/api/v1";
+const PRODUCTION_DOMAIN = process.env.NEXT_PUBLIC_PRODUCTION_DOMAIN;
+const DEVELOPMENT_DOMAIN = process.env.NEXT_PUBLIC_DEVELOPMENT_DOMAIN;
 
-export const DOMAIN = process.env.NODE_ENV === 'production' 
+export const DOMAIN =
+  process.env.NODE_ENV === "production"
     ? PRODUCTION_DOMAIN
     : DEVELOPMENT_DOMAIN;
